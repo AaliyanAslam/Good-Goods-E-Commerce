cb892cb369c8dcfcb18ea19b065ad9e02c1f58a5
AaliyanAslam <aaliyanaslam22@gmail.com>
2025-01-01 18:55:12 +0500

    some update

---
+++ app.js
@@ -0,0 +1,18 @@
+// let cart = document.getElementById('cart-logo');
+// let cartInfo = document.getElementById('cart-information');
+
+// cart.addEventListener('click', function (){
+// if(cartInfo.style.display == 'none'){
+
+//     cartInfo.style.display = 'block';
+
+// } else {
+
+//     cartInfo.style.display = 'none';
+
+// }
+
+
+// })
+
+
