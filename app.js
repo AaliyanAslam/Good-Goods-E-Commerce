2130008c785988e61bd94be819fd6133af192913
AaliyanAslam <aaliyanaslam22@gmail.com>
2025-01-01 19:17:51 +0500

    done

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
-// let cart = document.getElementById('cart-logo');
-// let cartInfo = document.getElementById('cart-information');
+let cartCount1 = 0;
+let cartCount2 = 0;
 
-// cart.addEventListener('click', function (){
-// if(cartInfo.style.display == 'none'){
-
-//     cartInfo.style.display = 'block';
-
-// } else {
-
-//     cartInfo.style.display = 'none';
-
-// }
+const cartCountElement = document.querySelector('.cart-count');
+const cartCountElement2 = document.querySelector('.cart-count-2');
+const alertPopup = document.querySelector('.alert-popup');
+const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
 
 
-// })
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    cartCount1++;
+    cartCountElement.textContent = cartCount1;
+    showAlertPopup();
+  });
+});
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    cartCount2++;
+    cartCountElement2.textContent = cartCount2;
+    showAlertPopup();
+  });
+});
+
+function showAlertPopup() {
+  alertPopup.classList.add('show');
+  setTimeout(() => {
+    alertPopup.classList.remove('show');
+  }, 2000);
+}
+
+
+// gif js
+
+const gif = document.getElementById('gif');
+const img = document.getElementById('img');
+
+// Add an event listener for the 'click' event
+gif.addEventListener('click', function () {
+  img.style.display = 'none';
+  gif.style.display = 'none';
+
+});
+
+
+
+
 
 
